2661c3d6b2b40c55ef05b8aa9c71732d5c0d80ac
Ajani Timothy <timothyinioluwa@gmail.com>
2024-03-12 01:09:18 +0100

    Update 2-print.py

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("Programming is like building a multilingual puzzle,\nfollowed by a new line.")
+print('"Programming is like building a multilingual puzzle,\nfollowed by a new line.\n')
